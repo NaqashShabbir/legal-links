ee9d8d0fbc6dcc0cb21806f1b33e228290bf6be4
Laiba3131 <129585341+Laiba3131@users.noreply.github.com>
2023-11-11 17:54:17 +0500

    doctor profile

---
+++ lib/src/lawyer_profile/view/login_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:legal_links_app/src/auth/vm/auth_vm.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../resources/resources.dart';
+import '../../../resources/validator.dart';
+import '../../../utils/bottom_sheets/forget_password_sheet.dart';
+import '../../../utils/common-widgets/custom_button.dart';
+import '../../../utils/common-widgets/custom_textformfield.dart';
+import '../../../utils/common-widgets/global_widget.dart';
+import '../../../utils/hights_widths.dart';
+import '../../base/view/base_view.dart';
+import 'signup_screen_one.dart';
+
+class LoginScreenOfLawyer extends StatefulWidget {
+  static String route = "/loginScreenofdoctor";
+
+  LoginScreenOfLawyer({super.key});
+
+  @override
+  State<LoginScreenOfLawyer> createState() => _LoginScreenOfDoctorState();
+}
+
+class _LoginScreenOfDoctorState extends State<LoginScreenOfLawyer> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController(
+      //text: 'ds@sss.ff'
+      );
+
+  TextEditingController passwordController = TextEditingController(
+      // text: 'a222222@'
+      );
+
+  FocusNode emailFocus = FocusNode();
+
+  FocusNode passwordFocus = FocusNode();
+
+  bool ispObscure = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthVM>(builder: (context, vm, _) {
+      return SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(vertical: 12.sp, horizontal: 12.sp),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(R.images.logo, height: 25.h),
+                  h2,
+                  Text(
+                    "Login",
+                    style: R.textStyles.poppinsBold(
+                      color: R.colors.primary,
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                  h2,
+                  CustomTextFormField(
+                    fieldTitle: "Email",
+                    controller: emailController,
+                    hintText: 'Enter email',
+                    focusNode: emailFocus,
+                    inputAction: TextInputAction.next,
+                    inputType: TextInputType.emailAddress,
+                    validator: FieldValidator.validateEmail,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                  ),
+                  CustomTextFormField(
+                    controller: passwordController,
+                    focusNode: passwordFocus,
+                    inputAction: TextInputAction.done,
+                    inputType: TextInputType.visiblePassword,
+                    validator: FieldValidator.validatePassword,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    hintText: 'Enter password',
+                    fieldTitle: "Password",
+                    obscureText: ispObscure,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          ispObscure = !ispObscure;
+                        });
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 8.sp),
+                        child: Icon(
+                          ispObscure
+                              ? Icons.visibility_off_rounded
+                              : Icons.remove_red_eye_rounded,
+                          color: Colors.grey,
+                          size: 16.sp,
+                        ),
+                      ),
+                    ),
+                  ),
+                  h2,
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: () {
+                        {
+                          // Get.bottomSheet(ChangePasswordSheet());
+                          Get.bottomSheet(
+                            const ForgotPasswordSheet(
+                              title: 'Forgot Password?',
+                              subTitle:
+                                  'Enter your reqistered Email ID We will send you a link to reset your password',
+                              labelText: 'Email',
+                              placeHolder: 'Enter email',
+                              text: 'Proceed',
+                            ),
+                            isScrollControlled: true,
+                          );
+                        }
+                      },
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "Forgot Password?",
+                            textAlign: TextAlign.center,
+                            style: R.textStyles.poppinsMedium(
+                              fontSize: 12.sp,
+                              color: R.colors.primary,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  h2,
+                  CustomButton(
+                    text: "Login",
+                    tap: () async {
+                      await login(vm);
+                    },
+                  ),
+                  h2,
+                ],
+              ),
+            ),
+          ),
+          bottomNavigationBar: Padding(
+            padding: EdgeInsets.only(bottom: 8.sp),
+            child: GlobalWidgets.authBottomWidget(
+              "Don't have an account?",
+              '  Sign Up As Lawyer',
+              () => Get.offAllNamed(SignupScreenOneOfLawyer.route),
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Future<void> login(AuthVM vm) async {
+    // if (_formKey.currentState!.validate()) {
+    Get.offAllNamed(BaseView.route);
+    // }
+  }
+}
---
+++ lib/src/lawyer_profile/view/signup_screen_two.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+import '../../../resources/resources.dart';
+import '../../../resources/validator.dart';
+import '../../../utils/common-widgets/app_decoration.dart';
+import '../../../utils/common-widgets/custom_textformfield.dart';
+import '../../../utils/hights_widths.dart';
+import '../model/lawyer_model.dart';
+import '../vm/signup_lawyer.dart';
+
+class SignupScreenTwoOfLawyer extends StatefulWidget {
+  static String route = '/signupTwoaslawyer';
+
+  const SignupScreenTwoOfLawyer({super.key});
+
+  @override
+  State<SignupScreenTwoOfLawyer> createState() =>
+      _SignupScreenTwoOfLawyerState();
+}
+
+class _SignupScreenTwoOfLawyerState extends State<SignupScreenTwoOfLawyer> {
+  FocusNode lawyerFocus = FocusNode();
+  TextEditingController lawyerController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Consumer<SignupLawyer>(builder: (context, vm, _) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(vertical: 12.sp, horizontal: 12.sp),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              h3,
+              Text('Step 1/5 About you'),
+              h1,
+              speciallistLawyerDropdown(vm: vm),
+              h1,
+              CustomTextFormField(
+                controller: lawyerController,
+                hintText: 'Any other Speciality',
+                focusNode: lawyerFocus,
+                inputAction: TextInputAction.next,
+                inputType: TextInputType.name,
+                validator: FieldValidator.validateEmpty,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+              ),
+              h1,
+            ]),
+          ),
+        );
+      }),
+    );
+  }
+
+  LawyerModel? laywersSpe;
+
+  Widget speciallistLawyerDropdown({required SignupLawyer vm}) {
+    return DropdownButtonFormField<LawyerModel?>(
+      borderRadius: BorderRadius.circular(8),
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      items: vm.castList
+          .map((item) => DropdownMenuItem<LawyerModel?>(
+                value: item,
+                child: Text(
+                  item.specialist ?? "",
+                  style: R.textStyles
+                      .poppinsRegular(color: R.colors.black, fontSize: 8.sp),
+                ),
+              ))
+          .toList(),
+      decoration: AppDecoration.fieldDecoration(hintText: "Select Specialist"),
+      value: laywersSpe,
+      validator: (value) {
+        if (value == null) {
+          return "required";
+        }
+        return null;
+      },
+      onChanged: (value) {
+        laywersSpe = value;
+      },
+    );
+  }
+
+  Widget categoryLawyerDropdown({required SignupLawyer vm}) {
+    return DropdownButtonFormField<LawyerModel?>(
+      borderRadius: BorderRadius.circular(8),
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      items: vm.castList
+          .map((item) => DropdownMenuItem<LawyerModel?>(
+                value: item,
+                child: Text(
+                  item.specialist ?? "",
+                  style: R.textStyles
+                      .poppinsRegular(color: R.colors.black, fontSize: 8.sp),
+                ),
+              ))
+          .toList(),
+      decoration: AppDecoration.fieldDecoration(hintText: "Select Specialist"),
+      value: laywersSpe,
+      validator: (value) {
+        if (value == null) {
+          return "required";
+        }
+        return null;
+      },
+      onChanged: (value) {
+        laywersSpe = value;
+      },
+    );
+  }
+}
