574348d72221402ed2f5c0636747e9fae8ddc383
hussnain <mhussnainshabbir90@gmail.com>
2023-11-18 16:19:20 +0500

    image picker service

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,6 +4,7 @@ import 'package:legal_links_app/src/auth/view/signup_screen.dart';
 import 'package:legal_links_app/src/base/view/base_view.dart';
 import 'package:legal_links_app/src/base/view/pages/settings/view/profile_screen.dart';
 import 'package:legal_links_app/src/base/view/pages/settings/view/rating_and_reviews_screen.dart';
+import 'package:legal_links_app/src/base/view/pages/settings/view/update_client_profile.dart';
 import '../src/base/view/pages/appointment/view/appointment_detail_screen.dart';
 import '../src/base/view/pages/appointment/view/appointment_view.dart';
 import '../src/base/view/pages/appointment/view/book_appointment_screen.dart';
@@ -30,43 +31,27 @@ abstract class AppPages {
     GetPage(name: LoginScreen.route, page: () => const LoginScreen()),
     GetPage(name: SignupScreen.route, page: () => const SignupScreen()),
     GetPage(name: BaseView.route, page: () => const BaseView()),
-    GetPage(
-        name: PrivacyPolicyScreen.route,
-        page: () => const PrivacyPolicyScreen()),
-    GetPage(
-        name: TermsAndConditions.route, page: () => const TermsAndConditions()),
-    GetPage(
-        name: RatingAndReviewsScreen.route,
-        page: () => const RatingAndReviewsScreen()),
+    GetPage(name: PrivacyPolicyScreen.route, page: () => const PrivacyPolicyScreen()),
+    GetPage(name: TermsAndConditions.route, page: () => const TermsAndConditions()),
+    GetPage(name: RatingAndReviewsScreen.route, page: () => const RatingAndReviewsScreen()),
     GetPage(name: ProfileScreen.route, page: () => const ProfileScreen()),
-    GetPage(
-        name: SignupScreenOneOfLawyer.route,
-        page: () => const SignupScreenOneOfLawyer()),
-    GetPage(
-        name: SignupScreenTwoOfLawyer.route,
-        page: () => const SignupScreenTwoOfLawyer()),
-    GetPage(
-        name: SignupScreenThreeOfLawyer.route,
-        page: () => const SignupScreenThreeOfLawyer()),
+    GetPage(name: SignupScreenOneOfLawyer.route, page: () => const SignupScreenOneOfLawyer()),
+    GetPage(name: SignupScreenTwoOfLawyer.route, page: () => const SignupScreenTwoOfLawyer()),
+    GetPage(name: SignupScreenThreeOfLawyer.route, page: () => const SignupScreenThreeOfLawyer()),
     GetPage(name: SignupScreenFour.route, page: () => const SignupScreenFour()),
     GetPage(name: CompleteProfile.route, page: () => const CompleteProfile()),
     GetPage(name: AppointmentView.route, page: () => const AppointmentView()),
     GetPage(name: AllLawyersScreen.route, page: () => const AllLawyersScreen()),
-    GetPage(
-        name: AllChambersScreen.route, page: () => const AllChambersScreen()),
+    GetPage(name: AllChambersScreen.route, page: () => const AllChambersScreen()),
     GetPage(name: AllCourtsScreen.route, page: () => const AllCourtsScreen()),
     GetPage(name: AllLawyersScreen.route, page: () => const AllLawyersScreen()),
-    GetPage(
-        name: AppointmentDetails.route, page: () => const AppointmentDetails()),
+    GetPage(name: AppointmentDetails.route, page: () => const AppointmentDetails()),
     GetPage(name: LawyerDetailsScrren.route, page: () => LawyerDetailsScrren()),
-    GetPage(
-        name: BookAppointmentScreen.route,
-        page: () => const BookAppointmentScreen()),
+    GetPage(name: BookAppointmentScreen.route, page: () => const BookAppointmentScreen()),
     GetPage(name: LawyerBaseView.route, page: () => const LawyerBaseView()),
     GetPage(name: LawyerHomeView.route, page: () => const LawyerHomeView()),
-    GetPage(name: UserRattingView.route, page: () => UserRattingView()),
-    GetPage(
-        name: ScheduleAppointmentView.route,
-        page: () => ScheduleAppointmentView()),
+    GetPage(name: UserRattingView.route, page: () => const UserRattingView()),
+    GetPage(name: ScheduleAppointmentView.route, page: () => const ScheduleAppointmentView()),
+    GetPage(name: UpdateClientScreen.route, page: () => const UpdateClientScreen()),
   ];
 }
---
+++ lib/src/base/view/pages/settings/view/update_client_profile.dart
@@ -0,0 +1,291 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:legal_links_app/constants/enums.dart';
+import 'package:legal_links_app/resources/resources.dart';
+import 'package:legal_links_app/resources/validator.dart';
+import 'package:legal_links_app/src/auth/model/user_model.dart';
+import 'package:legal_links_app/src/auth/vm/auth_vm.dart';
+import 'package:legal_links_app/utils/common-widgets/custom_button.dart';
+import 'package:legal_links_app/utils/common-widgets/custom_textformfield.dart';
+import 'package:legal_links_app/utils/common-widgets/global_widget.dart';
+import 'package:legal_links_app/utils/hights_widths.dart';
+import 'package:legal_links_app/utils/zbot_toast.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+class UpdateClientScreen extends StatefulWidget {
+  static String route = "/UpdateClientScreen";
+  const UpdateClientScreen({super.key});
+
+  @override
+  State<UpdateClientScreen> createState() => _UpdateClientScreenState();
+}
+
+class _UpdateClientScreenState extends State<UpdateClientScreen> {
+  showSnackBar(context, text) {
+    var snackBar = SnackBar(content: Text(text));
+    ScaffoldMessenger.of(context).showSnackBar((snackBar));
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController confirmpasswordController = TextEditingController();
+  TextEditingController dateCon = TextEditingController();
+
+  DateTime? selectedDate;
+
+  FocusNode emailFocus = FocusNode();
+  FocusNode passwordFocus = FocusNode();
+  FocusNode confirmpasswordFocus = FocusNode();
+  FocusNode nameFocus = FocusNode();
+  FocusNode dateFocus = FocusNode();
+  FocusNode genderFn = FocusNode();
+
+  bool isObscure1 = false;
+  bool isObscure2 = false;
+
+  PhoneNumber number = PhoneNumber(isoCode: 'PK');
+  TextEditingController phoneNumberController = TextEditingController();
+  FocusNode numberFN = FocusNode();
+
+  DateTime? currentBackPressTime;
+
+  @override
+  Widget build(BuildContext context) {
+    // return Consumer2<AuthVM, RootProvider>(builder: (context, authVm, vm, _) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: GlobalWidgets.appBar(
+          "Update Profile",
+          onTap: () {
+            Get.back();
+          },
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 12.sp, horizontal: 12.sp),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                h3,
+                CustomTextFormField(
+                  fieldTitle: "Full Name",
+                  controller: nameController,
+                  hintText: 'Enter name',
+                  focusNode: nameFocus,
+                  inputAction: TextInputAction.next,
+                  inputType: TextInputType.name,
+                  validator: FieldValidator.validateEmpty,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                CustomTextFormField(
+                  fieldTitle: "Email",
+                  controller: emailController,
+                  hintText: 'Enter email',
+                  focusNode: emailFocus,
+                  inputAction: TextInputAction.next,
+                  inputType: TextInputType.emailAddress,
+                  validator: FieldValidator.validateEmail,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                h1,
+                Container(
+                  margin: EdgeInsets.only(left: 4.sp, bottom: 4.sp, top: 6.sp),
+                  child: Text(
+                    "Phone Number",
+                    style: R.textStyles.poppinsMedium(
+                      fontSize: 11.sp,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                phoneNumberField(),
+                h1,
+                CustomTextFormField(
+                  controller: passwordController,
+                  focusNode: passwordFocus,
+                  inputAction: TextInputAction.next,
+                  inputType: TextInputType.visiblePassword,
+                  validator: FieldValidator.validatePassword,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  hintText: 'Enter password',
+                  fieldTitle: "Password",
+                  obscureText: isObscure1,
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isObscure1 = !isObscure1;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 8.sp),
+                      child: Icon(
+                        isObscure1 ? Icons.visibility_off_rounded : Icons.remove_red_eye_rounded,
+                        color: Colors.grey,
+                        size: 16.sp,
+                      ),
+                    ),
+                  ),
+                ),
+                CustomTextFormField(
+                  controller: confirmpasswordController,
+                  focusNode: confirmpasswordFocus,
+                  inputAction: TextInputAction.done,
+                  inputType: TextInputType.visiblePassword,
+                  validator: (val) => FieldValidator.validatePasswordMatch(
+                      confirmpasswordController.text, passwordController.text),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  hintText: 'Enter confirm password',
+                  fieldTitle: "Confirm Password",
+                  obscureText: isObscure2,
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isObscure2 = !isObscure2;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 8.sp),
+                      child: Icon(
+                        isObscure2 ? Icons.visibility_off_rounded : Icons.remove_red_eye_rounded,
+                        color: Colors.grey,
+                        size: 16.sp,
+                      ),
+                    ),
+                  ),
+                ),
+                h3,
+                CustomButton(
+                  buttonTitle: "Sign up",
+                  tap: () async {
+                    await buttonFn();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget phoneNumberField() {
+    return InternationalPhoneNumberInput(
+      focusNode: numberFN,
+      inputDecoration: InputDecoration(
+        isDense: true,
+        suffixIcon: Icon(
+          Icons.phone_outlined,
+          color: numberFN.hasFocus ? R.colors.primary : R.colors.grey,
+        ),
+        hintText: 'Number',
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 17,
+        ),
+        filled: true,
+        focusColor: R.colors.primary,
+        hintStyle: R.textStyles.poppinsRegular(fontSize: 11.sp, color: Colors.grey),
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              width: 0.5,
+              color: R.colors.red,
+            )),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              width: 0.5,
+              color: R.colors.primary,
+            )),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              width: 1,
+              color: R.colors.primary,
+            )),
+        focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(
+              width: 0.5,
+              color: R.colors.red,
+            )),
+      ),
+      onInputChanged: (PhoneNumber phonenumber) {
+        number = phonenumber;
+      },
+      onInputValidated: (val) {
+        debugPrint(val.toString());
+      },
+      selectorConfig: const SelectorConfig(
+          leadingPadding: 10,
+          selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
+          showFlags: false,
+          setSelectorButtonAsPrefixIcon: true,
+          trailingSpace: true),
+      spaceBetweenSelectorAndTextField: 0,
+      selectorTextStyle: TextStyle(color: R.colors.primary),
+      ignoreBlank: false,
+      autoValidateMode: AutovalidateMode.onUserInteraction,
+      initialValue: number,
+      textFieldController: phoneNumberController,
+      // validator: (value) => FieldValidator.validatePhoneNumber(
+      //     phoneNumberController.text.trim(), context),
+      formatInput: false,
+      keyboardAction: TextInputAction.done,
+      keyboardType: const TextInputType.numberWithOptions(signed: true, decimal: true),
+      inputBorder: const UnderlineInputBorder(),
+      onSaved: (PhoneNumber number) {
+        debugPrint('On Saved: $number');
+      },
+
+      onFieldSubmitted: (value) {
+        setState(() {});
+        FocusScope.of(context).requestFocus(passwordFocus);
+      },
+    );
+  }
+
+  Future<void> buttonFn() async {
+    if (_formKey.currentState!.validate()) {
+      Timestamp now = Timestamp.now();
+      UserModel createClient = UserModel(
+        role: context.read<AuthVM>().userRole,
+        fullName: nameController.text.trim(),
+        createdAt: now,
+        updatedAt: now,
+        phoneNumber: PhoneNumberModel(
+          number: phoneNumberController.text.trim(),
+          isoCode: number.isoCode,
+          countryCode: number.dialCode,
+        ),
+        //  id: ,
+        email: emailController.text.trim(),
+        status: UserStatus.ACTIVE,
+      );
+
+      await context.read<AuthVM>().signUp(createClient, passwordController.text.trim());
+
+      // debugPrint(" body: ");
+      // debugPrint('role: ${context.read<AuthVM>().userRole}');
+      // debugPrint('fullName: ${nameController.text.trim()}');
+      // debugPrint('createdAt: $now');
+      // debugPrint('updatedAt: $now');
+      // Uncomment the line below if `id` is a property
+      // debugPrint('id: $id');
+      // debugPrint('phoneNumberController: ${phoneNumberController.text.trim()}');
+      // debugPrint('number.isoCode: ${number.isoCode}');
+      // debugPrint('number.dialCode: ${number.dialCode}');
+      // debugPrint('email: ${emailController.text.trim()}');
+      // debugPrint('status: ${UserStatus.ACTIVE}');
+    }
+  }
+}
