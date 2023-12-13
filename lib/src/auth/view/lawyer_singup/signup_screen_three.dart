2067ab475d312c024ec151cacb1d8964659121e0
hussnain <mhussnainshabbir90@gmail.com>
2023-12-13 11:43:58 +0500

    user statuses

--- lib/src/auth/view/lawyer_singup/signup_screen_three.dart
+++ lib/src/auth/view/lawyer_singup/signup_screen_three.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, prefer_typing_uninitialized_variables
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -41,8 +41,6 @@ class _SignupScreenThreeOfLawyerState extends State<SignupScreenThreeOfLawyer> {
   LatLng? latLng;
   PickLocationData? pickLocationData;
 
-  var duration;
-
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmpasswordController = TextEditingController();
 
@@ -53,11 +51,6 @@ class _SignupScreenThreeOfLawyerState extends State<SignupScreenThreeOfLawyer> {
   bool isObscure2 = false;
 
   FocusNode aboutFocus = FocusNode();
-  @override
-  void initState() {
-    super.initState();
-    duration = const Duration(minutes: 30); // Initialize with a default value
-  }
 
   @override
   Widget build(BuildContext context) {
